82a2dff6090cb4fc37b9eb02f502f69be903b37b
willda <willda@yandex.ru>
2017-09-28 00:53:32 +0500

    fix import

--- core/base.py
+++ core/base.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException, WebDriverException
 
-from cian_tests import config
+from .. import config
 
 
 # Экспортируем только Page и Element
