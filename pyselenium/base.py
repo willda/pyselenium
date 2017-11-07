a783e7ca96de05c5c1b87fe8928b33888fb6df44
George Fortunatov <qweeeze@gmail.com>
2017-11-08 03:50:10 +0500

    api improvements

--- pyselenium/base.py
+++ pyselenium/base.py
@@ -22,6 +22,10 @@ class Page:
         self.driver = driver
         self._visit_child_elements()
 
+    @property
+    def title(self):
+        return self.driver.title
+
     def _visit_child_elements(self, parent_el=None):
         """
         Рекурсивно обходит все дочерние элементы и сохраняет их иерархию
@@ -32,6 +36,7 @@ class Page:
             if not isinstance(child_el, Element):
                 continue
             child_el.parent_el = parent_el
+            child_el.page = self
             self._visit_child_elements(child_el)
 
     def open(self):
@@ -57,10 +62,72 @@ class Element:
             self.locator = by, locator
         # Ссылка на родительский элемент
         self.parent_el = None
+        self.page = None
+        self._index = 0
+        self._i = 0
+
+        for k, v in vars(type(self)).items():
+            if isinstance(v, Element):
+                el = v.clone()
+                el.parent_el = self
+                vars(self)[k] = el
+
+    def clone(self):
+        new_el = self.__class__(*reversed(self.locator))
+        new_el.page = self.page
+        new_el.parent_el = self.parent_el
+        return new_el
+
+    def __getitem__(self, item):
+        # todo: slice
+        el = self.clone()
+        el._index = item
+        return el
+
+    @property
+    def _find_root(self):
+        page = self.page
+        if isinstance(self.parent_el, Element):
+            self.parent_el.raise_if_not_found()
+            find_root = self.parent_el.element
+        else:
+            find_root = page.driver
+        return find_root
+
+    @property
+    def _elements_list(self):
+        return self.wait_elements()
+
+    def wait_elements(self, condition=None, timeout=config.WAIT_TIMEOUT):
+        page = self.page
+        while not isinstance(page, Page):
+            page = page.parent_el
+        if isinstance(self.parent_el, Element):
+            self.parent_el.raise_if_not_found()
+            find_root = self.parent_el.element
+        else:
+            find_root = page.driver
+
+        if condition is None:
+            condition = EC.presence_of_all_elements_located(self.locator)
+        try:
+            return WebDriverWait(find_root, timeout).until(condition)
+        except TimeoutException:
+            pass
+
+
+
+    def raise_if_not_found(self):
+        if not self.element:
+            raise RuntimeError('Element not found by {}: {}'.format(*self.locator))
+
 
     @property
     def element(self):
-        return self._wait_element()
+        try:
+            return self._elements_list[self._index]
+        except IndexError:
+            pass
 
     def _wait_element(self, condition=None, timeout=config.WAIT_TIMEOUT):
         """
@@ -72,12 +139,12 @@ class Element:
         while not isinstance(page, Page):
             page = page.parent_el
         if isinstance(self.parent_el, Element):
-            self.raise_if_not_found(self.parent_el)
+            self.parent_el.raise_if_not_found()
             find_root = self.parent_el.element
         else:
             find_root = page.driver
         if condition is None:
-            condition = EC.presence_of_element_located(self.locator)
+            condition = EC.presence_of_all_elements_located(self.locator)
         try:
             return WebDriverWait(find_root, timeout).until(condition)
         except TimeoutException:
@@ -91,8 +158,9 @@ class Element:
         button = Element('button[@attr="name"]')
         button.click()```
         """
-        self.raise_if_not_found()
-        return getattr(self.element, item)
+        if not item.startswith('_'):
+            self.raise_if_not_found()
+            return getattr(self.element, item)
 
     def __set__(self, instance, value):
         """
@@ -109,14 +177,9 @@ class Element:
         """
         return bool(self.element)
 
-    def raise_if_not_found(self, elem=None):
-        elem = self if elem is None else elem
-        if not elem.element:
-            raise RuntimeError('Element not found by {}: {}'.format(*elem.locator))
-
     def hover(self):
         self.raise_if_not_found()
-        return ActionChains(self.driver).move_to_element(self.element).perform()
+        return ActionChains(self.page.driver).move_to_element(self.element).perform()
 
     def wait_until_clickable(self, timeout=config.WAIT_TIMEOUT):
         self._wait_element(EC.element_to_be_clickable, timeout)
@@ -125,3 +188,22 @@ class Element:
     def wait_until_visible(self, timeout=config.WAIT_TIMEOUT):
         self._wait_element(EC.visibility_of_element_located, timeout)
         return self
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return len(self._elements_list)
+
+    def __next__(self):
+        rv = self[self._i]
+        if self._i < len(self):
+            self._i += 1
+        else:
+            self._i = 0
+            raise StopIteration
+        return rv
+
+    def __contains__(self, item):
+        if isinstance(item, Element):
+            return item.element in self._elements_list
